3bc07ae6ad8388d77af329af5968812bcd76bbe3
moustaouiSalaheddine <moustaoui.salaheddine@gmail.com>
2022-12-14 19:06:16 +0100

    CategoryController

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::with('parent')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $category = new Category;
+        $category->name = $request->input('name');
+
+        // If you choose a parent category
+        if ($request->filled('parent_id')) {
+            $category_parent = Category::find($request->input('parent_id'));
+            $category->parent()->associate($category_parent);
+        }
+
+        $category->save();
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+        $category->name = $request->input('name');
+
+        // If you choose a parent category
+        if ($request->filled('parent_id') && $request->input('parent_id') != $category->parent_id) {
+            $category_parent = Category::find($request->input('parent_id'));
+            $category->parent()->associate($category_parent);
+        }
+
+        $category->save();
+        return response()->json($category, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return response()->json(null, 204);
+    }
+}
